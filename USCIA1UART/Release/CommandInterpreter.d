9fe2c2d86eae504f363ff0e0851dd721a6ec8fea
Jamie Boyd <jadobo@gmail.com>
2022-01-21 00:03:32 -0800

    oops

---
+++ USCIA1UART/Release/CommandInterpreter.d
@@ -0,0 +1,45 @@
+# FIXED
+
+CommandInterpreter.obj: ../CommandInterpreter.c
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/ccs_base/msp430/include/msp430.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/ccs_base/msp430/include/msp430f5529.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/ccs_base/msp430/include/in430.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+CommandInterpreter.obj: ../USCIcmdInterpreter.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdio.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdarg.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h
+CommandInterpreter.obj: /Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h
+
+../CommandInterpreter.c:
+
+/Applications/ti/ccs1010/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs1010/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+/Applications/ti/ccs1010/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+../USCIcmdInterpreter.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdio.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdarg.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h:
+
---
+++ USCIA1UART/Release/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5529.cmd 
+
+C_SRCS += \
+../CommandInterpreter.c \
+../USCIcmdInterpreter.c 
+
+C_DEPS += \
+./CommandInterpreter.d \
+./USCIcmdInterpreter.d 
+
+OBJS += \
+./CommandInterpreter.obj \
+./USCIcmdInterpreter.obj 
+
+OBJS__QUOTED += \
+"CommandInterpreter.obj" \
+"USCIcmdInterpreter.obj" 
+
+C_DEPS__QUOTED += \
+"CommandInterpreter.d" \
+"USCIcmdInterpreter.d" 
+
+C_SRCS__QUOTED += \
+"../CommandInterpreter.c" \
+"../USCIcmdInterpreter.c" 
+
+
